04aff76f02504cccd79469130edfd056073494d4
fercascue <fercascue@gmail.com>
2022-12-22 08:02:38 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from scratch
+          Learn React from scratch ssssss
         </a>
       </header>
     </div>
